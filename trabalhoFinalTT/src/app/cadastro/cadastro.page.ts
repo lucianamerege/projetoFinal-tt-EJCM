6a2ebb173b9aa2e597a8fe36b1a229335ded580b
LucianaMerege <lucianama@dcc.ufrj.br>
2020-02-04 09:30:41 -0300

    + pagina de cadastro

---
+++ trabalhoFinalTT/src/app/cadastro/cadastro.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.page.html',
+  styleUrls: ['./cadastro.page.scss'],
+})
+export class CadastroPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
