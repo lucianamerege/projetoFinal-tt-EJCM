2054084208c4413ab27fbd7d8e3413e4e7573828
LucianaMerege <lucianama@dcc.ufrj.br>
2020-02-06 17:13:37 -0300

    *css e html da pagina de cadastro

--- trabalhoFinalTT/src/app/cadastro/cadastro.page.ts
+++ trabalhoFinalTT/src/app/cadastro/cadastro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, ReactiveFormsModule} from '@angular/forms';
+import { FormControl, ReactiveFormsModule, FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,9 +9,26 @@ import { FormControl, ReactiveFormsModule} from '@angular/forms';
 })
 export class CadastroPage implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(public formbuilder: FormBuilder, private router: Router) {
+    this.registerForm = this.formbuilder.group({
+      name: [null, [Validators.required, Validators.minLength(5)]],
+      email:[null, [Validators.required, Validators.email]],
+      password:[null,[Validators.required, Validators.minLength(5)]],
+      passwordConfirm:[null,[Validators.required]]
+    });
+  }
+
+  submitForm(form){
+    console.log(form.value);
+  }
 
   ngOnInit() {
+  }
+
+  cadastrado(){
+    this.router.navigate(['/tabs/tab1']);
   }
 
 }
